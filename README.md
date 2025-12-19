bc536e7d7c2e374677e265017b0ddc80f01b4431
Fanesz <fanes23.pratama@mhs.mdp.ac.id>
2025-12-19 15:34:46 +0700

    edit: migrate to gemini 2.5

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Nindy AI
 
-Integrated with gemini-2.0-flash.
+Integrated with gemini-2.5-flash.
 Currently only text support, not support image read/generate.
--- external/gemini.go
+++ external/gemini.go
@@ -38,7 +38,7 @@ func InitializeGemini() {
 		return
 	}
 	currentClient = client
-	currentModel = client.GenerativeModel("gemini-2.0-flash")
+	currentModel = client.GenerativeModel("gemini-2.5-flash")
 	currentModel.SetTemperature(0.7)
 	currentModel.SetMaxOutputTokens(1000)
 
